ffc96bfd5febe510574d12907d205b229912d266
ahmad <ahmad.ruzaini@ums.edu.my>
2025-03-21 22:53:06 +0800

    initial commit

--- frappe_migrate_x/__init__.py
+++ frappe_migrate_x/__init__.py
@@ -1,3 +1,49 @@
+import importlib
+import os
+import frappe
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
+patches_loaded = False
+
+
+def console(*data):
+    frappe.publish_realtime("out_to_console", data, user=frappe.session.user)
+
+
+def load_monkey_patches():
+
+    global patches_loaded
+
+    for app in frappe.get_installed_apps():
+
+        if app in ["frappe", "erpnext"]:
+            continue
+
+        folder = frappe.get_app_path(app, "monkey_patches")
+        if not os.path.exists(folder):
+            continue
+
+        for module_name in os.listdir(folder):
+            if not module_name.endswith(".py") or module_name == "__init__.py":
+                continue
+
+            importlib.import_module(f"{app}.monkey_patches.{module_name[:-3]}")
+
+    patches_loaded = True
+
+
+connect = frappe.connect
+
+
+def custom_connect(*args, **kwargs):
+    out = connect(*args, **kwargs)
+
+    if frappe.conf.auto_commit_on_many_writes:
+        frappe.db.auto_commit_on_many_writes = 1
+
+    load_monkey_patches()
+    return out
+
+
+frappe.connect = custom_connect
---
+++ frappe_migrate_x/tests/test_migrate.py
@@ -0,0 +1,60 @@
+import importlib
+import os
+import frappe
+from frappe.tests.utils import FrappeTestCase
+from frappe.utils import cint
+
+patches_loaded = False
+
+"""bench --site <sitename> run-tests --module frappe_migrate_x.tests.test_migrate --test <test_case_name>"""
+class TestMigrateX(FrappeTestCase):
+
+    def test_init(self):
+        print("hello world")
+
+    def test_is_monkey_patches_folder_exist(self):
+        my_app = ["frappe_migrate_x"]
+        for app in frappe.get_installed_apps():
+            if app in my_app:
+                folder = frappe.get_app_path(app, "monkey_patches")
+                print(folder)
+                if os.path.exists(folder):
+                    print("")
+                    output = "monkey patches exists"
+                    for module_name in os.listdir(folder):
+                        if not module_name.endswith(".py") or module_name == "__init__.py":
+                            continue
+
+                        importlib.import_module(f"{app}.monkey_patches.{module_name[:-3]}")
+            output = "does not exist"
+
+        patches_loaded = True
+                    
+        self.assertTrue("monkey patches exists" in output)
+
+
+
+    def test_load_monkey_patches(self):
+
+        global patches_loaded
+
+        # print(patches_loaded)
+       
+
+        for app in frappe.get_installed_apps():
+            print(app)
+            if app in ["frappe", "erpnext"]:
+                continue
+
+            folder = frappe.get_app_path(app, "monkey_patches")
+            if not os.path.exists(folder):
+                print(app+"skipping monkey patches")
+                continue
+
+        #     for module_name in os.listdir(folder):
+        #         if not module_name.endswith(".py") or module_name == "__init__.py":
+        #             continue
+
+        #         importlib.import_module(f"{app}.monkey_patches.{module_name[:-3]}")
+
+        # patches_loaded = True
